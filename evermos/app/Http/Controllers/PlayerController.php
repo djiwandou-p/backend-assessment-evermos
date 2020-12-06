668a2be1ac94ddfa8e06d403b62073e3f483465d
Rana Krisna <ranakrisna17031995@gmail.com>
2020-12-06 14:41:42 +0700

    Tennis Player API

---
+++ evermos/app/Http/Controllers/PlayerController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Player;
+use Illuminate\Http\Request;
+use App\Http\Resources\PlayerResource;
+use Illuminate\Support\Facades\Validator;
+
+class PlayerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $models = Player::all();
+        return response([ 'models' => PlayerResource::collection($models), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'name' => 'required|max:191',
+        ]);
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors(), 'Validation Error']);
+        }
+        $models = Player::create($data);
+        return response(['models' => new PlayerResource($models), 'message' => 'Created successfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Player $player)
+    {
+        return response(['models' => new PlayerResource($player), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Player $player)
+    {
+        $player->update($request->all());
+        return response(['models' => new PlayerResource($player), 'message' => 'Update successfully'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Player $player)
+    {
+        $player->delete();
+        return response(['message' => 'Deleted']);
+    }
+}
--- evermos/routes/api.php
+++ evermos/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlayerController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function (Request $request) {
+    return "Hai";
+});
+Route::apiResource('player', PlayerController::class);
